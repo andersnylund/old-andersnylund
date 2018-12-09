0dde532a1d548bb7a907ca6a98325d40b545ffa4
Anders Nylund <anders.nylund.an@gmail.com>
2018-12-09 12:47:12 +0200

    Remove unnessecary files. Install material-ui

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import Layout from '../components/layout';
-import Image from '../components/image';
+import Typography from '@material-ui/core/Typography';
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
+  <div>
+    <Typography variant="h1">Hi people</Typography>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </div>
 );
 
 export default IndexPage;
