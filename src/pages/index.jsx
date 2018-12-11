866aa51fe112e1a501db535bb4d1037b286bfec8
Anders Nylund <anders.nylund.an@gmail.com>
2018-12-11 20:57:46 +0200

    Start implementation of first design

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components';
 
-export const StyledDiv = styled.div`
-  text-align: center;
+import github from '../images/github.svg';
+
+const Container = styled.div`
+  color: #fcbfb7;
+`;
+
+const TopContainer = styled.div`
+  background: #8aa1b1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 50%;
+  position: fixed;
+`;
+
+const BottomContainer = styled.div`
+  background: #33261d;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50%;
+  position: fixed;
+`;
+
+const FontContainer = styled.div`
+  margin: 2rem;
 `;
 
 const IndexPage = () => (
-  <StyledDiv>
-    <Typography variant="h1">Anders Nylund</Typography>
-  </StyledDiv>
+  <Container>
+    <TopContainer>
+      <FontContainer>
+        <h1>Anders Nylund</h1>
+      </FontContainer>
+      <img src={github} alt="GitHub logo" />
+    </TopContainer>
+    <BottomContainer />
+  </Container>
 );
 
 export default IndexPage;
