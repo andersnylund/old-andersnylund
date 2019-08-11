da7daf30fdeee21ae1ab5a06dc1dd3236710e177
Anders Nylund <anders.nylund.an@gmail.com>
2019-08-11 20:43:50 +0300

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-145417753-2',
+        head: false,
+      },
+    },
   ],
 };
