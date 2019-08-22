58e7206159898535957073f6d5ede0e70da9e8be
Anders Nylund <anders.nylund.an@gmail.com>
2019-08-22 18:43:24 +0300

    Revert
    This reverts commit da7daf30fdeee21ae1ab5a06dc1dd3236710e177.

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,12 +30,5 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-145417753-2',
-        head: false,
-      },
-    },
   ],
 };
